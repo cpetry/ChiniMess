4edcfc97116893db7e138aa80c9ae2488439b678
Christian Petry <petry2christian@gmail.com>
2013-04-22 12:46:43 +0200

    added ProjectNotes and ClassDiagram in Visio

---
+++ ProjectNotes.md
@@ -0,0 +1,9 @@
+- class Board
+- 
+moveNumber is number like it should be printed:
+moveNumber gets incremented each round not each move 
+
+the columns will be labeled a, b, c, d, e
+and the rows (from bottom / white) 1, 2, 3, 4, 5, 6.
+
+ Capital letters are white pieces
--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
+
 /* COPYRIGHT (C) 2013 Christian Petry. All Rights Reserved. */ 
 public class Board {
+    private final int WIDTH = 5, HEIGHT = 6;
+    
+    
+    ArrayList<Character> arr = new ArrayList<Character>();
 
+    public void getField(int x, int y){
+        arr.get(x + y * WIDTH);
+    }
+    
+    public int getWidth(){
+        return this.WIDTH;
+    }
 }
