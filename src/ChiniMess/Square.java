9f46fb5e1d36d179125aa51f17858d473e7a590f
michaelbanck <banckmichael@gmail.com>
2013-04-22 15:06:09 +0200

    class Square.java length of the given string

--- src/ChiniMess/Square.java
+++ src/ChiniMess/Square.java
@@ -9,26 +9,32 @@ public class Square {
 		
 		char [] chararray = koordinaten.toCharArray();
 		
-		switch(chararray[0]){
-		
-			case 'a' : col = 0; 
-			case 'b' : col = 1; 
-			case 'c' : col = 2; 
-			case 'd' : col = 3; 
-			case 'e' : col = 4; 
+		if(chararray.length == 2){
 			
-			default : col = -1;
-		}
-		switch(chararray[1]){
-		
-			case '1' : row = 0;
-			case '2' : row = 1;
-			case '3' : row = 2;
-			case '4' : row = 3;
-			case '5' : row = 4;
-			case '6' : row = 5;
+			switch(chararray[0]){
 			
-			default : row = -1;
+				case 'a' : col = 0; 
+				case 'b' : col = 1; 
+				case 'c' : col = 2; 
+				case 'd' : col = 3; 
+				case 'e' : col = 4; 
+				
+				default : col = -1;
+			}
+			switch(chararray[1]){
+			
+				case '1' : row = 0;
+				case '2' : row = 1;
+				case '3' : row = 2;
+				case '4' : row = 3;
+				case '5' : row = 4;
+				case '6' : row = 5;
+				
+				default : row = -1;
+			}
+		}else{
+			col = -1;
+			row = -1;
 		}
 	}
 	
