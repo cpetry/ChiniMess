1f63b6643beec6862cb9be9a0fc60a40574a0c03
Christian Petry <petry2christian@gmail.com>
2013-04-22 16:01:32 +0200

    added SquareTests

--- src/ChiniMess/SquareTest.java
+++ src/ChiniMess/SquareTest.java
@@ -10,15 +10,32 @@ public class SquareTest {
     public void equalConstructorTest() {
         Square s1 = new Square("a1");
         Square s2 = new Square(0, 0);
-        
         assertEquals("should be equal", s1, s2);
-        //fail("Not yet implemented");
+
+        s1 = new Square("b2");
+        s2 = new Square(1, 1);
+        assertEquals("should be equal", s1, s2);
+        
+        s1 = new Square("c3");
+        s2 = new Square(2, 2);
+        assertEquals("should be equal", s1, s2);
+        
+        s1 = new Square("d4");
+        s2 = new Square(3, 3);
+        assertEquals("should be equal", s1, s2);
+        
+        s1 = new Square("e5");
+        s2 = new Square(4, 4);
+        assertEquals("should be equal", s1, s2);
     }
     
-    public void equalToString(){
-        Square s1 = new Square("a1");
-        
-        assertEquals(s1, "a1");
+    @Test
+    public void equalToString(){      
+        assertEquals(new Square("a1").toString(), "a1");
+        assertEquals(new Square("b2").toString(), "b2");
+        assertEquals(new Square("c3").toString(), "c3");
+        assertEquals(new Square("d4").toString(), "d4");
+        assertEquals(new Square("e5").toString(), "e5");
     }
 
 }
