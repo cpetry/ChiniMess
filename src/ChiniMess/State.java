bb80409681bc49cc70c28c4a97a5438ff5558411
Christian Petry <petry2christian@gmail.com>
2013-04-23 14:21:43 +0200

    renamed Score to State added Game class and random player

--- src/ChiniMess/State.java
+++ src/ChiniMess/State.java
@@ -2,7 +2,7 @@ package ChiniMess;
 
 import Figures.Figure;
 
-public class Score {
+public class State {
     public static int calculateScore(Board b){
         int score = 0;
         for (int r = 0; r < 6; r++)
@@ -20,5 +20,24 @@ public class Score {
                     score -= f.getScore();
             }
         return score;
+    }
+    
+    public static boolean gameOver(Board b){
+        boolean white_lives = false, black_lives = false;
+        for (int r = 0; r < 6; r++)
+            for (int c = 0; c < 5; c++){
+                Square s = new Square(c, r);
+                if (!s.isValid())
+                    throw new IllegalArgumentException("wrong square!");
+                Figure f = b.getFigureFromField(s);
+                if (f.toString() == "k")
+                    black_lives = true;
+                else if (f.toString() == "K")
+                    white_lives = true;
+            }
+        if (black_lives && white_lives)
+            return false;
+        else
+            return true;
     }
 }
