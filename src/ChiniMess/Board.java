8d619082ff7a6f9f5f8f68560b02b763c3f86e57
olfus <olfthebolf@gmail.com>
2013-04-23 08:22:17 +0200

    added input and output for boards and logic for move on the board

--- src/ChiniMess/Board.java
+++ src/ChiniMess/Board.java
@@ -2,8 +2,10 @@
 
 package ChiniMess;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 import Figures.*;
 
@@ -18,25 +20,41 @@ public class Board {
     ArrayList<Figure> board = new ArrayList<Figure>();
 
     public Board() {
-    	moveNumber = 0;
-    	onMove = false; 	
-    	initEmptyBoard();
-    	initDefaultFigurePosition();
+    	moveNumber = 1; 			 // first move is one
+    	onMove = true;  			 //white gets first 		
+    	initEmptyBoard(); 			 //set empty board
+    	initDefaultFigurePosition(); //set start positions
     	
     }
     
-	public Board(String inputArgument) {
-		moveNumber = 0;
-    	onMove = false; 	
-    	initEmptyBoard();
-    	analyseArgument(inputArgument);
-		
+	public Board(String boardInput) {
+		this();
+    	if (checkAndSetBoardFromInput(boardInput) == false) { //check and set input
+    		initDefaultFigurePosition(); 					  //set start positions if input is rubbish!
+    		moveNumber = 1; 								  // first move is one
+        	onMove = true;  								  //white gets first 
+    	}
+	}
+	
+	public Board(BufferedReader  bf) throws IOException { // don't forget ExceptionHandling!
+		this();
+		String strLine; // buffer for current line
+		String inputStream = "";
+				
+		while ((strLine = bf.readLine()) != null)   { //Read File Line By Line
+			inputStream += strLine;
+		}
+		if (checkAndSetBoardFromInput(inputStream) == false) { //check and set input
+    		initDefaultFigurePosition(); 					   //set start positions if input is rubbish!
+    		moveNumber = 1; 							       // first move is one
+        	onMove = true;  								   //white gets first 
+    	}
 	}
 	
 	
 	private void initEmptyBoard() {
 		for (int i = 0; i < WIDTH * HEIGHT; i++) {
-				this.board.add(null);
+				this.board.add(null); //put an empty figure in each cell
 		}
 		
 	}
@@ -44,20 +62,184 @@ public class Board {
 	
 	
 	public String toString() {
-		String outputString = ""; 
+		String outputString = "";
+			
+		outputString += "" + this.moveNumber + " "; //print number of moves first
+		
+		//set header
+		if (this.onMove == true) //white 
+				outputString += "W"; //white-players turn
+		else
+				outputString += "B"; //black-players turn
+
+		outputString += "\n"; //new line for board
+		
+		//set board
 		for (int i = 0; i < board.size(); i++) {
-			if (i % WIDTH == 0)
-				outputString += '\n';
+			if (i % WIDTH == 0 && i != 0) 
+				outputString += '\n'; //end of one line
 			
 			if (board.get(i) != null) {
-				outputString += board.get(i);
+				outputString += board.get(i); //print the toString() of the figure
 			}
 			else {
-				outputString += ".";
+				outputString += "."; //for empty positions
 			}
 			
 		}
-		return outputString;
+		return outputString; //return the result
+	}
+	
+	
+	
+	public boolean checkAndSetMoves(String inputMove) {
+		//check moves
+		try {
+			int moves = Integer.valueOf(inputMove);
+			if (moves < MAXMOVIES  && moves >= 0 ) {	//check if move is in range	
+				this.moveNumber = moves; 				//set attribute to current move_value
+				return true;							//return success
+			}
+			return false;								//BAM!
+			
+					
+		}
+		//set default if no luck
+		catch(NumberFormatException e_ref) {
+			return false;								//BAM!
+		}
+		
+	}
+	public boolean checkAndSetColor(String input) {
+		
+		
+		//check colors
+		if (input.contains("W") && input.contains("B")) {;
+			return false; 					// current move cannot be black and white!
+		}
+		else if (input.contains("W")) {
+			this.onMove = true; 			//set white player
+		}
+		else if (input.contains("B"))
+			this.onMove = false; 			//set black player
+		else
+			return false;					//BAM! 
+		
+		return true;						//YEAH!
+		
+	}
+	
+	public boolean checkAndSetBoardFromInput(String input) {
+		
+		boolean boolShit; 
+		boolShit = true;
+		
+		//init StringTokenizer -> remove spaces
+		String tokens [] = input.split("[ \t\n]+"); //split by regular expression
+		if (tokens[0] != null && this.checkAndSetMoves(tokens[0]) == false)	//first part of the String has to be the move_value
+				return false;	
+		
+		if (tokens[1] != null && this.checkAndSetColor(tokens[1]) == false) 
+				return false;
+		
+
+		if (tokens.length == HEIGHT + 2) { //test if String_HEIGHT is valid
+			for (int i = 2; i < tokens.length; i++) {
+				char currentLine [] = tokens[i].toCharArray();
+				
+				if (currentLine.length < WIDTH) //test String_WIDTH is valid
+					return false;
+				
+				int offset = (i - 2) * WIDTH;	//-2 for counting from zero
+				for (int j = 0; j < currentLine.length;j++) {
+					if (validateAndSetCurrentFigure(currentLine[j],offset + j) == false) { //check and set current Figure_Object
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public boolean validateAndSetCurrentFigure(char c, int index) {
+		boolean isWhite = false; 
+		boolean possible = true; // return_value
+		
+		if (c > 'A' && c < 'Z') //white is upper_Case
+			isWhite = true; 
+		
+		c = Character.toLowerCase(c); //we do not need to check upper and lower-cases
+		
+		try { //set the objects
+			switch (c) {
+				
+				case 'k':
+					board.set(index, new King(isWhite));
+					break;
+				case 'q':
+					board.set(index, new Queen(isWhite));
+					break;
+				case 'b':
+					board.set(index, new Bishop(isWhite));
+					break;
+				case 'n':
+					board.set(index, new Knight(isWhite));
+					break;
+				case 'r':
+					board.set(index, new Rook(isWhite));
+					break;
+				case 'p':
+					board.set(index, new Pawn(isWhite));
+					break;
+				case '.':
+					board.set(index,null);
+					break;
+				default: 
+					possible = false;
+			}
+		}
+		//if index is out of range
+		catch (IndexOutOfBoundsException e_ref) {
+			possible = false;
+		}
+		//return success
+		return possible;
+		
+	}
+	
+	public boolean setFigureToField(Figure inputFigure, Square inputSquare) {
+		
+
+		for (int i = 0; i < board.size(); i++) {
+			if (board.get(i).equals(inputFigure) && inputSquare.isValid()) { //TODO: check for possible moves
+												 //&& inputFigure.checkMove(
+												//		 new Move("" + i % WIDTH + i / HEIGHT +  
+														 	   //  + inputSquare.getCol() + inputSquare.getRow()))) {
+				board.set((1 + inputSquare.getRow()) * inputSquare.getCol(), inputFigure); //set figure on board -> remember offset
+				board.set(i,null); //fromSquare is empty, now!
+				return true; 	   // return true by success
+			}
+		}
+		return false; // BAM!
+	}
+	
+	public Figure getFigureFromField(Square inputSquare) {
+		if (inputSquare.isValid()) {
+			return board.get((1 + inputSquare.getRow()) * inputSquare.getCol()); //get Figure from Square
+			
+		}
+		return null;
+	}
+	
+	public void print(BufferedWriter bw) throws IOException { //BAM-> ExceptionHandling outside the class!
+		String output [] = this.toString().split("\n"); //BufferedWriter ignores '\n'
+		for (String line: output) {
+			bw.write(line); //write board to file
+			bw.newLine();	//and another line...
+		}
+		bw.close(); //close stream
 	}
 	
 	private void  initDefaultFigurePosition() {
@@ -76,10 +258,8 @@ public class Board {
 		for (int i = WIDTH; i < 2 * WIDTH; i++) {
 			board.set(i, new Pawn(false));
 		}
-			
-		
+
 		//set figures for black
-		
 		offset = WIDTH * HEIGHT - 1;
 		board.set(offset - 0, new King(true)); 
 		board.set(offset - 1, new Queen(true));
@@ -92,57 +272,7 @@ public class Board {
 		for (int i = offset - 2 * WIDTH; i < offset - WIDTH; i++) {
 			board.set(i, new Pawn(true));
 		}
-		
-		
-		
-	}
-	private void analyseArgument(String inputArgument) {
-		
-		//check for right-turn
-		String currentToken = null;
-		StringTokenizer tokenizer = new StringTokenizer(inputArgument, " ");
-		if (tokenizer.hasMoreElements()) {
-			currentToken = (String) tokenizer.nextElement();
-		}
-		
-		if (currentToken == "B") {
-			this.onMove = false;
-		}
-		else if (currentToken == "W") {
-			this.onMove = true;
-		}
-		else {
-			initDefaultFigurePosition();
-			return;
-		}
-		//check for turn 	
-		if (tokenizer.hasMoreElements()) {
-			currentToken = (String) tokenizer.nextElement();
-		}
-		//try to get current move from string
-		try {
-			int moves = new Integer(currentToken);
-			if (moves < MAXMOVIES && moves >= 0  
-				&& (moves % 2 == 0 && this.onMove)			//test for white-players turn 
-				&& (moves % 2 == 1 && !this.onMove)) {		//test for black-players turn 
-				this.moveNumber = moves;
-			}
 			
-		}
-		//set default if no luck
-		catch(NumberFormatException e_ref) {
-			initDefaultFigurePosition();
-		}
-		
-		//set figure_positions 
-		//validateFigureChars();
-		
-		
-	}
-	
-	public static void main (String args []) {
-		Board b = new Board(); 
-		System.out.println(b);
 	}
 	
 }
---
+++ src/ChiniMess/BoardTest.java
@@ -0,0 +1,124 @@
+package ChiniMess;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import Figures.Figure;
+
+public class BoardTest{
+	
+	Board dummyBoard; 
+	
+	public BoardTest() {
+	}
+	
+	@Test
+	public void validColorTest() {
+		dummyBoard = new Board ();
+        assertEquals("should be equal",dummyBoard.checkAndSetColor("W"),true);
+	}
+	
+	@Test
+	public void invalidColorTest() {
+		dummyBoard = new Board ();
+        assertEquals("should be equal",dummyBoard.checkAndSetColor("C"),false);
+	}
+	
+	@Test
+	public void validMoveTest() {
+		dummyBoard = new Board ();
+        assertEquals("should be equal",dummyBoard.checkAndSetMoves("22"),true);
+	}
+	
+	@Test
+	public void invalidMoveTest() {
+		dummyBoard = new Board ();
+        assertEquals("should be equal",dummyBoard.checkAndSetMoves("2dd2"),false);
+	}
+	
+	@Test
+	public void validBoardInputTest() {
+		
+		dummyBoard = new Board();
+		String input = generateValidInput();
+		assertEquals("should be equal",dummyBoard.checkAndSetBoardFromInput(input),true);
+	}
+	
+	@Test
+	public void validFigure() {
+		dummyBoard = new Board();
+		char figure = 'k'; //white king
+		int offset = 0;
+		int position = 2;
+		assertEquals("should be equal",dummyBoard.validateAndSetCurrentFigure(figure,offset + position),true);
+	}
+
+	@Test	
+	public void invalidFigure() {
+		dummyBoard = new Board();
+		char figure = 'k'; //white king
+		int offset = 0;
+		int position = 2222;
+		assertEquals("should be equal",dummyBoard.validateAndSetCurrentFigure(figure,offset + position),false);
+	}
+	
+	@Test
+	public void testOutputStream() {
+		dummyBoard = new Board();
+		try {
+			dummyBoard.print(new BufferedWriter(new FileWriter("output.txt")));
+			
+		} 
+		catch (IOException ex) {
+	            ex.printStackTrace();
+		}
+		assertEquals("should be equal",false,false); //TODO: @useful testCase
+	}
+	
+	@Test
+	public void testPositionChange() {
+		dummyBoard = new Board(); //init standard-positions
+		Square fromSquare = new Square("b1");
+		Square toSquare = new Square("c5");
+		boolean valid = false;
+		
+		Figure inputFigure = dummyBoard.getFigureFromField(fromSquare);
+		
+		dummyBoard.setFigureToField(inputFigure, toSquare);
+		
+		if (dummyBoard.getFigureFromField(toSquare).equals(inputFigure)) {
+			valid = true;
+		}
+		assertEquals("should be equal",valid,true); 
+	}
+	
+	
+	//helper_methods
+	public String generateValidInput() {
+		
+		String outputString = "22 B  \n"
+							+ "kqbnr\n"
+							+ "ppppp\n"
+							+ ".....\n"
+							+ ".....\n"
+							+ "PPPPP\n"
+							+ "RNBQK\n";
+		return outputString;
+	}
+	
+	public String generateInitPositions() {
+		String outputString =     "1 W  \n"
+								+ "kqbnr\n" 
+								+ "ppppp\n"
+								+ ".....\n"
+								+ ".....\n"
+								+ "PPPPP\n"
+								+ "RNBQK\n";
+		return outputString;
+	}
+}
